1b7e1a2dac5718b903a6a84d4949240190cfeaeb
Julian Gruber <julian@juliangruber.com>
2015-01-07 20:11:28 +0100

    add .watch(el, fn)

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+var debounce = require('debounce');
 
-module.exports = function inViewport(el) {
+module.exports = inViewport;
+
+function inViewport(el){
   var rect = el.getBoundingClientRect();
   var innerHeight = window.innerHeight || document.documentElement.clientHeight;
   var innerWidth = window.innerWidth || document.documentElement.clientWidth;
@@ -8,5 +11,25 @@ module.exports = function inViewport(el) {
     && rect.left >= 0
     && rect.bottom <= innerHeight
     && rect.right <= innerWidth;
+};
+
+inViewport.watch = function(el, fn){
+  var state = false;
+  var check = function(){
+    var cur = inViewport(el);
+    if (cur != state) {
+      state = cur;
+      fn(state);
+    }
+  };
+  var immediate = true;
+  var onscroll = debounce(check, 100, immediate);
+
+  document.addEventListener('scroll', onscroll);
+  check();
+
+  return function unwatch(){
+    document.removeEventListener('scroll', onscroll);
+  };
 };
 
